064fc89e1e426539fef5cfcb7543b8744d94e49b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-04-28 22:08:41 -0300

    [28/04/2023] Correção de Zeros

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿void frase(string txt)
+﻿    int i;
+void frase(string txt)
 {
     Console.ForegroundColor = ConsoleColor.DarkYellow;
     for (int i = 0; i < txt.Length; i++)
@@ -17,21 +18,31 @@ if (dvd == 0)
 {
     dvd = 1;
 }
+if (vl == 0)
+{
+    vl = 1;
+}
 decimal total = vl % dvd;
 if (total == 0)
 {
-    frase($"O número {vl} é multiplo de {dvd}.");
+    frase($"\nO número {vl} é multiplo de {dvd}.\n");
+    frase($"\nCalculando a quantidade necessário para chegar em {vl}.\n");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("Pressione qualquer tecla para continuar");
+    Console.ReadKey();
+    Console.Clear();
+    Console.WriteLine("[]_Calculo____________");
+    for (i = 1; (dvd*i) != vl; i++)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine($"|| {dvd} x {i} = {dvd*i}");
+    //Thread.Sleep(30);
+}
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine($"[] {dvd} x {i} = {(dvd*i)}");
 }
 else if (total != 0)
 {
-    frase($"O número {vl} NÃO é multiplo de {dvd}.");
+    frase($"\nO número {vl} NÃO é multiplo de {dvd}.\n");
 }
-frase($"Quantidade necessário para chegar à {vl}.\n");
-int i;
-for (i = 1; (dvd*i) != vl; i++)
-{
-    Console.ForegroundColor = ConsoleColor.DarkCyan;
-    Console.WriteLine($"{dvd} x {i} = {dvd*i}");
-}
-Console.WriteLine($"{dvd} x {i} = {dvd*i}");
 Console.ResetColor();
